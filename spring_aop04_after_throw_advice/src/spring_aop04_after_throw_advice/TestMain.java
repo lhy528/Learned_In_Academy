91194583a94774e6fae9295d53f98dc22578ad78
사용자 <@HY-Gram>
2020-05-21 13:19:21 +0900

    스프링-AOP-AfterAdvice로 예외 catch

---
+++ spring_aop04_after_throw_advice/src/spring_aop04_after_throw_advice/TestMain.java
@@ -0,0 +1,16 @@
+package spring_aop04_after_throw_advice;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TestMain {
+	public static void main(String[] args) {
+		Message msg = new GenericXmlApplicationContext("app.xml").getBean("proxyBean", Message.class);
+		msg.printMsg();
+		
+		try {
+			msg.printThrowMessage();
+		}catch (IllegalArgumentException e) {
+			System.out.println("캐치됨");
+		}
+	}
+}
