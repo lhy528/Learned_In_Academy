ba853b1585748f82aac5796198b8d4aa422c193c
사용자 <@HY-Gram>
2020-05-21 13:00:46 +0900

    property 대신 p:로 DI

--- spring_aop02_after_returning_advice/src/spring_aop02_after_returning_advice/AfterInterceptorAdvice.java
+++ spring_aop02_after_returning_advice/src/spring_aop02_after_returning_advice/AfterInterceptorAdvice.java
@@ -9,6 +9,7 @@ public class AfterInterceptorAdvice implements AfterReturningAdvice {
 	@Override
 	public void afterReturning(Object returnValue, Method method, Object[] args,
 			Object target) throws Throwable {
+
 		System.out.println("---------------------");
 		System.out.println("메인 업무 메소드 호출 후에");
 		System.out.println("실행되는 메소드");
