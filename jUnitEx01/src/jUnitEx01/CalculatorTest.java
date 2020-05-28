bbe61070a423d297ea9233a776190b46bc7f21b0
사용자 <@HY-Gram>
2020-05-28 15:18:27 +0900

    jUnit을 이용한 단위테스트 기초 및 예제

---
+++ jUnitEx01/src/jUnitEx01/CalculatorTest.java
@@ -0,0 +1,31 @@
+package jUnitEx01;
+
+public class CalculatorTest {
+   public static void main(String[] args) {
+      Calculator c = new Calculator();
+      
+      double result = c.sum(10, 20);
+      
+      
+      
+//      if(result != 30) {
+//          System.err.println("err: "+result);
+//       }
+       
+       result = c.sum(1.1, 2);
+       
+//       if(result !=3.1) {
+//          System.err.println("err: "+result);
+//       }
+       
+       result = c.sum(1.1, -2);
+       
+//       if(result != -0.9) {
+//    	   System.err.println("err: " +result); // result=-0.8999999999
+//       }
+       
+       if(result <= -0.89 && result >=-0.9) {
+    	   System.err.println("err : "+result);
+       }
+   }
+} 
